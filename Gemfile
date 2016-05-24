472ff322abb7f1006eeec0991fc882257808579f
Shunjiro Yatsuzuka <syatsuzuka@oss-erp.jp>
2016-05-24 12:47:56 -0700

    Closes #1

--- Gemfile
+++ Gemfile
@@ -24,6 +24,12 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+# RestClient
+gem 'mime-types'
+gem 'netrc'
+gem 'http-cookie'
+gem 'rest-client'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,14 @@
+require 'rest-client'
+
 class HomeController < ApplicationController
+
+	def index
+		response = RestClient.get 'https://blockchain.info/charts/market-price?showDataPoints=false&timespan=all&show_header=true&daysAverageString=1&scale=0&format=json&address='
+
+		@data = response.to_str
+		@test = "test"
+
+		print "%s\n"
+		print "%s\n" % @data
+	end
 end
